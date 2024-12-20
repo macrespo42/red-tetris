e4438a227209ad55fed916980f20346313f8a8c3
macrespo42 <macrespo@student.42.fr>
2024-12-20 15:47:53 +0100

    feat: Base jsx setup

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
         <h1 className="main-title">RED-TETRIS</h1>
       </header>
       <section className="home-section">
-        <input type="text" name="room-name" value="" placeholder="room" />
-        <input type="text" name="player-name" value="" placeholder="pseudo" />
+        <input type="text" name="room-name" placeholder="room" />
+        <input type="text" name="player-name" placeholder="pseudo" />
         <Button text="CREATE ROOM" to="/" />
         <Button text="JOIN GAME" to="/" />
       </section>
--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,5 +1,24 @@
+import { useParams } from "react-router";
+import Button from "./Button";
+
 const Room = () => {
-  return <h1>Hello from room</h1>;
+  const { room } = useParams();
+  const players = ["player1", "player2", "lorem ipsum"];
+  return (
+    <div className="room-page">
+      <header>
+        <h1 className="main-title">{room}</h1>
+      </header>
+      <section className="player-list">
+        <ul>
+          {players.map((player) => (
+            <li key={player}>{player}</li>
+          ))}
+        </ul>
+      </section>
+      <Button text="START" to="/" />
+    </div>
+  );
 };
 
 export default Room;
