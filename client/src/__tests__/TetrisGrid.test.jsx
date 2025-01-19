94449bf70fbf4ce53f52460452fb053c8073bb2f
mpochard <88907375+m3L4n@users.noreply.github.com>
2025-01-19 23:49:53 +0100

    feat : add some test : coverage of ~92%

--- client/src/__tests__/TetrisGrid.test.jsx
+++ client/src/__tests__/TetrisGrid.test.jsx
@@ -16,7 +16,9 @@ describe("room view", () => {
     const home = render(
       <TetrisGrid matrix={[[1,1,1], [0, 0, 0],[2,2,2]]}/>
     )
-    expect(screen.getByText("room_test")).toBeDefined()
+    const cellsBefore = screen.getAllByRole("gameboard");
+    expect(cellsBefore[1].className).toEqual("cell yellow")
+    expect(cellsBefore[4].className).toEqual("cell ")
   home.unmount()
   })
 })
--- client/src/__tests__/useMoveTetrominoes.test.js
+++ client/src/__tests__/useMoveTetrominoes.test.js
@@ -1,6 +1,66 @@
-import { describe, it, expect } from "vitest";
-import playerReducer, { all, socketId, name, roomName, isGameOwner } from "../playerSlice";
+import { describe, it, expect, beforeEach, render } from "vitest";
+import { vi } from "vitest";
+import { socket } from "../socket";
+import useMoveTetrominoes from "../hooks/useMoveTetrominoes";
+import { renderHook } from "@testing-library/react";
+vi.mock("../socket", () => ({
+  socket: {
+    emit: vi.fn(),
+  },
+}));
 
-describe("useMoveTetrominoes", () => {
-  it("", () => {});
+describe("useMoveTetrominoes Hook", () => {
+  let addEventListenerSpy;
+  let removeEventListenerSpy;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    addEventListenerSpy = vi.spyOn(document, "addEventListener");
+    removeEventListenerSpy = vi.spyOn(document, "removeEventListener");
+  });
+
+  it(" moove ArrowLeft", () => {
+    renderHook(() => useMoveTetrominoes({ id: "player1" }));
+    const event = new KeyboardEvent("keydown", { code: "ArrowLeft" });
+    document.dispatchEvent(event);
+    expect(socket.emit).toHaveBeenCalledWith("move left", "player1");
+    // home.unmount();
+  });
+  it(" moove ArrowRight", () => {
+    renderHook(() => useMoveTetrominoes({ id: "player1" }));
+    const event = new KeyboardEvent("keydown", { code: "ArrowRight" });
+    document.dispatchEvent(event);
+    expect(socket.emit).toHaveBeenCalledWith("move right", "player1");
+  });
+  it(" moove ArrowDown", () => {
+    renderHook(() => useMoveTetrominoes({ id: "player1" }));
+    const event = new KeyboardEvent("keydown", { code: "ArrowDown" });
+    document.dispatchEvent(event);
+    expect(socket.emit).toHaveBeenCalledWith("move down", "player1");
+  });
+  it(" moove ArrowUp", () => {
+    renderHook(() => useMoveTetrominoes({ id: "player1" }));
+    const event = new KeyboardEvent("keydown", { code: "ArrowUp" });
+    document.dispatchEvent(event);
+    expect(socket.emit).toHaveBeenCalledWith("rotate", "player1");
+  });
+  it(" moove Space", () => {
+    renderHook(() => useMoveTetrominoes({ id: "player1" }));
+    const event = new KeyboardEvent("keydown", { code: "Space" });
+    document.dispatchEvent(event);
+    expect(socket.emit).toHaveBeenCalledWith("move bottom", "player1");
+  });
+  it(" moove doesnt exist", () => {
+    renderHook(() => useMoveTetrominoes({ id: "player1" }));
+    const event = new KeyboardEvent("keydown", { code: "dddd" });
+    document.dispatchEvent(event);
+    expect(socket.emit).not.toHaveBeenCalled();
+  });
+
+  it(" moove return remove event listener", () => {
+    const { unmount } = renderHook(() => useMoveTetrominoes({ id: "player1" }));
+    expect(addEventListenerSpy).toHaveBeenCalledWith("keydown", expect.any(Function));
+    unmount();
+    expect(removeEventListenerSpy).toHaveBeenCalledWith("keydown");
+  });
 });
