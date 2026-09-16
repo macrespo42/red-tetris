93a2ee35cc41eaf27550872ab8f495e9351f50b5
macrespo42 <macrespo@student.42.fr>
2026-09-16 15:41:18 +0200

    feat(use_case): soft/hard drop piece use_cases

--- server/use_cases/soft_drop_piece.py
+++ server/use_cases/soft_drop_piece.py
@@ -1,2 +1,42 @@
+from dataclasses import dataclass
+
+from domain.entities.game import Game
+from domain.repositories.game_repository import IGameRepository
+
+
+@dataclass
+class SoftDropInput:
+    game_id: str
+    player_id: str
+
+
+@dataclass
+class SoftDropOutput:
+    game: Game
+    moved: bool
+
+
 class SoftDropPiece:
-    pass
+    def __init__(self, game_repository: IGameRepository):
+        self._game_repository = game_repository
+
+    def execute(self, input: SoftDropInput) -> SoftDropOutput:
+        game = self._game_repository.get(input.game_id)
+        if game is None:
+            raise ValueError(
+                f"Game {input.game_id} not found"
+            )  # TODO create custom exception
+
+        player = game.get_player(input.player_id)
+        if player is None:
+            raise ValueError(
+                f"Player {input.player_id} not found in game {input.game_id}"
+            )
+
+        dx, dy = (0, 1)
+        moved = player.try_move(dx, dy)
+
+        if moved:
+            self._game_repository.save(game)
+
+        return SoftDropOutput(game=game, moved=moved)
